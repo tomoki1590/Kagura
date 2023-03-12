49b02efd4cd148d215824f6a18b6850fec5149c0
tomato <kaguratomato1@gmail.com>
2023-03-12 23:59:41 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) => MaterialApp(
